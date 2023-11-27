a1b9e5c922c16101b1d650f7f0a3ff0372411832
Haris Dautovic <haris.dautovic.dev@gmail.com>
2023-11-27 21:53:41 +0100

    Refactor permissions
    - Unrelated tests were moved to MainViewModelTests.
    - Improved naming.

--- presentation/src/test/java/com/hd/presentation/permissions/PermissionsViewModelTest.kt
+++ presentation/src/test/java/com/hd/presentation/permissions/PermissionsViewModelTest.kt
@@ -1,7 +1,6 @@
 package com.hd.presentation.permissions
 
 import android.content.Intent
-import android.view.View
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth
 import com.hd.data.permissions.mapper.toPermissions
@@ -9,14 +8,10 @@ import com.hd.data.permissions.model.PermissionDTO
 import com.hd.data.permissions.model.PermissionTypeDTO
 import com.hd.data.permissions.model.PermissionsDTO
 import com.hd.domain.permissions.usecase.PermissionsUseCase
-import com.hd.present.permissions.mapper.toUI
 import com.hd.presentation.observeOnce
-import com.hd.presentation.permissions.model.PermissionTypeUI
-import com.hd.presentation.permissions.model.PermissionUI
-import com.hd.presentation.permissions.model.event.NavigationEvent
-import com.hd.presentation.permissions.model.event.TaskEvent
-import com.hd.presentation.permissions.model.event.UiVisibilityEvent
-import com.hd.presentation.permissions.model.uistate.PermissionsUiState
+import com.hd.presentation.permissions.mapper.PermissionTypeUI
+import com.hd.presentation.permissions.mapper.PermissionUI
+import com.hd.presentation.permissions.mapper.toUI
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
@@ -58,15 +53,14 @@ class PermissionsViewModelTest {
     @Test
     fun `Init permissionsState should return Loading`() = runTest {
         // Act
-        val result = viewModel.permissionsFlow.first()
+        val result = viewModel.permissionsState.first()
 
         // Assert
-        Truth.assertThat(result.permissionsState).isEqualTo(PermissionsUiState.Loading)
+        Truth.assertThat(result).isEqualTo(PermissionsUiState.Loading)
     }
 
-    // Tests Feature: Permissions
     @Test
-    fun `checkAllPermissions whenAllPermissionsGranted should return ScheduleNotifications`() =
+    fun `checkAllPermissions whenAllPermissionsGranted should return Granted`() =
         runTest {
             // Arrange
             val permissionsResponseMock = listOf(
@@ -84,13 +78,12 @@ class PermissionsViewModelTest {
             val expectedPermissionsUiMock = permissionsMock.toUI().permissions
 
             coEvery { permissionUseCase.checkPermissions() } returns flowOf(permissionsMock)
-            coEvery { viewModel.permissionsActivityDismissed } returns flowOf(true)
 
             // Act
             viewModel.checkAllPermissions()
 
             // Assert
-            val actualPermissions = viewModel.permissionsFlow.first().permissionsState.let {
+            val actualPermissions = viewModel.permissionsState.first().let {
                 (it as PermissionsUiState.Success)
             }.permissionsUi
 
@@ -98,98 +91,11 @@ class PermissionsViewModelTest {
             Truth.assertThat(actualPermissions.permissions)
                 .containsExactlyElementsIn(expectedPermissionsUiMock)
             Truth.assertThat(actualPermissions.shouldAskPermission).isFalse()
-
-            viewModel.navigationEvent.observeOnce {
-                val expected = TaskEvent.ScheduleNotifications
-                Truth.assertThat(it).isInstanceOf(expected::class.java)
-            }
+            Truth.assertThat(actualPermissions.allGranted).isTrue()
         }
 
     @Test
-    fun `checkAllPermissions whenPermissionsAreNotGranted and not dismissed should return ShowPermissionsActivity`() =
-        runTest {
-            // Arrange
-            val permissionsResponseMock = listOf(
-                PermissionDTO(PermissionTypeDTO.AUTO_START, granted = false, isOptional = true),
-                PermissionDTO(PermissionTypeDTO.ALARM, granted = false, isOptional = false),
-                PermissionDTO(PermissionTypeDTO.NOTIFICATIONS, granted = false, isOptional = false)
-            )
-
-            val permissionsMock = PermissionsDTO(
-                permissions = permissionsResponseMock,
-                shouldAskPermission = true,
-                allGranted = false
-            ).toPermissions()
-
-            val expectedPermissions = permissionsMock.toUI().permissions
-
-            coEvery { permissionUseCase.checkPermissions() } returns flowOf(permissionsMock)
-            coEvery { viewModel.permissionsActivityDismissed } returns flowOf(false)
-
-            // Act
-            viewModel.checkAllPermissions()
-
-            // Assert
-            val actualPermissions = viewModel.permissionsFlow.first().permissionsState.let {
-                (it as PermissionsUiState.Success)
-            }.permissionsUi
-
-            Truth.assertThat(actualPermissions.permissions)
-                .containsExactlyElementsIn(expectedPermissions)
-            Truth.assertThat(actualPermissions.allGranted).isFalse()
-            Truth.assertThat(actualPermissions.shouldAskPermission).isTrue()
-
-            viewModel.navigationEvent.observeOnce {
-                val expected = NavigationEvent.ShowPermissionsActivity
-                Truth.assertThat(it).isInstanceOf(expected::class.java)
-            }
-        }
-
-
-    @Test
-    fun `checkAllPermissions whenPermissionsAreNotGranted and dismissed and shouldNotAskPermission should return MissingPermissionWarning`() =
-        runTest {
-            // Arrange
-            val permissionsResponseMock = listOf(
-                PermissionDTO(PermissionTypeDTO.AUTO_START, granted = false, isOptional = true),
-                PermissionDTO(PermissionTypeDTO.ALARM, granted = false, isOptional = false),
-                PermissionDTO(PermissionTypeDTO.NOTIFICATIONS, granted = false, isOptional = false)
-            )
-
-            val permissionsMock = PermissionsDTO(
-                permissions = permissionsResponseMock,
-                shouldAskPermission = false,
-                allGranted = false
-            ).toPermissions()
-
-            val expectedPermissions = permissionsMock.toUI().permissions
-
-            coEvery { permissionUseCase.checkPermissions() } returns flowOf(permissionsMock)
-            coEvery { viewModel.permissionsActivityDismissed } returns flowOf(true)
-
-            // Act
-            viewModel.checkAllPermissions()
-
-            // Assert
-            val actualPermissions = viewModel.permissionsFlow.first().permissionsState.let {
-                (it as PermissionsUiState.Success)
-            }.permissionsUi
-
-            Truth.assertThat(actualPermissions.permissions)
-                .containsExactlyElementsIn(expectedPermissions)
-            Truth.assertThat(actualPermissions.allGranted).isFalse()
-            Truth.assertThat(actualPermissions.shouldAskPermission).isFalse()
-
-            viewModel.uiVisibilityEvent.observeOnce {
-                val expected = UiVisibilityEvent.MissingPermissionWarning(View.VISIBLE)
-                Truth.assertThat(it).isInstanceOf(expected::class.java)
-                Truth.assertThat(it.visibility).isEqualTo(View.VISIBLE)
-            }
-        }
-
-
-    @Test
-    fun `handlePermissionToggle with AUTO_START should return OpenAutoStartActivity`() = runTest {
+    fun `handlePermissionToggle with AUTO_START should call OpenAutoStartActivity`() = runTest {
         // Arrange
         val intent = Intent()
         val permission = PermissionUI(
@@ -205,13 +111,13 @@ class PermissionsViewModelTest {
 
         // Assert
         viewModel.navigationEvent.observeOnce {
-            val expected = NavigationEvent.OpenAutoStartActivity(permission.intent!!)
+            val expected = PermissionsNavigation.OpenAutoStartActivity(permission.intent!!)
             Truth.assertThat(it).isInstanceOf(expected::class.java)
         }
     }
 
     @Test
-    fun `handlePermissionToggle with ALARM return shouldOpenAlarmActivity`() = runTest {
+    fun `handlePermissionToggle with ALARM should call OpenAlarmActivity`() = runTest {
         // Arrange
         val permission = PermissionUI(
             permissionType = PermissionTypeUI.ALARM,
@@ -224,13 +130,13 @@ class PermissionsViewModelTest {
 
         // Assert
         viewModel.navigationEvent.observeOnce {
-            val expected = NavigationEvent.OpenAlarmActivity
+            val expected = PermissionsNavigation.OpenAlarmActivity
             Truth.assertThat(it).isInstanceOf(expected::class.java)
         }
     }
 
     @Test
-    fun `handlePermissionToggle with NOTIFICATIONS and granted should return OpenAppSettings`() =
+    fun `handlePermissionToggle with NOTIFICATIONS and granted should call OpenAppSettings`() =
         runTest {
             // Arrange
             val permission = PermissionUI(
@@ -244,13 +150,13 @@ class PermissionsViewModelTest {
 
             // Assert
             viewModel.navigationEvent.observeOnce {
-                val expected = NavigationEvent.OpenAppSettings
+                val expected = PermissionsNavigation.OpenAppSettings
                 Truth.assertThat(it).isInstanceOf(expected::class.java)
             }
         }
 
     @Test
-    fun `handlePermissionToggle with NOTIFICATIONS and not granted should return RequestNotificationPermission`() =
+    fun `handlePermissionToggle with NOTIFICATIONS and !granted should call RequestNotificationPermission`() =
         runTest {
             // Arrange
             val permission = PermissionUI(
@@ -264,26 +170,13 @@ class PermissionsViewModelTest {
 
             // Assert
             viewModel.navigationEvent.observeOnce {
-                val expected = NavigationEvent.RequestNotificationPermission
+                val expected = PermissionsNavigation.RequestNotificationPermission
                 Truth.assertThat(it).isInstanceOf(expected::class.java)
             }
         }
 
-    // Tests Feature: Do not ask me again
     @Test
-    fun `showDoNotRemindMeAgain should return DoNotRemindMe`() = runTest {
-        // Act
-        viewModel.showDoNotRemindMeAgain()
-
-        // Assert
-        viewModel.uiVisibilityEvent.observeOnce { event ->
-            Truth.assertThat(event).isInstanceOf(UiVisibilityEvent.DoNotRemindMe::class.java)
-            Truth.assertThat(event.visibility).isEqualTo(View.VISIBLE)
-        }
-    }
-
-    @Test
-    fun `doNotAskMeAgain should call permissionsUseCase doNotAskMeAgain`() = runTest {
+    fun `doNotAskMeAgain should call permissionsUseCase doNotAskMePermissions`() = runTest {
         // Arrange
         val isChecked = true
         coEvery { permissionUseCase.doNotAskMePermissions(isChecked) } returns Unit
@@ -293,17 +186,5 @@ class PermissionsViewModelTest {
 
         // Assert
         coVerify { permissionUseCase.doNotAskMePermissions(isChecked) }
-    }
-
-
-    // Tests Feature: Dismiss
-    @Test
-    fun `dismissPermissionsActivity should call permissionsActivityDismissed`() = runTest {
-        // Act
-        viewModel.dismiss()
-
-        // Assert
-        val result = viewModel.permissionsActivityDismissed.first()
-        Truth.assertThat(result).isTrue()
     }
 }
