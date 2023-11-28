6a6c48b7b1bbdb26f7a934956d74d162fdc710d7
Haris Dautovic <haris.dautovic.dev@gmail.com>
2023-11-28 18:44:41 +0100

    Convert domain to kotlin library

--- domain/src/main/java/com/hd/domain/permissions/model/PermissionsDomain.kt
+++ domain/src/main/java/com/hd/domain/permissions/model/PermissionsDomain.kt
@@ -1,10 +1,8 @@
 package com.hd.domain.permissions.model
 
-import android.content.Intent
-
 data class Permission(
     val permissionType: PermissionType,
-    val intent: Intent?,
+    val intent: PermissionComponent?,
     val granted: Boolean
 )
 
@@ -12,6 +10,10 @@ data class Permissions(
     val permissions: List<Permission>,
     val shouldAskPermission: Boolean,
     val allGranted: Boolean
+)
+data class PermissionComponent(
+    val packageName: String,
+    val className: String
 )
 
 enum class PermissionType {
